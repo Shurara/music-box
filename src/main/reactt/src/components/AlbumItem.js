4333529a7bad28f5fbe1542e65cfc762378d9392
Oleksandr <o.rodiuk@gmail.com>
2018-04-21 14:51:22 +0300

    adde styles to albumItem

--- src/main/reactt/src/components/AlbumItem.js
+++ src/main/reactt/src/components/AlbumItem.js
@@ -16,25 +16,27 @@ class AlbumItem extends React.Component {
         .then(res=>res.json())
         .then((data) => this.setState({album: data}))
         .then(() => this.setState({tracks:this.state.album.tracks.map(track =>
-                <div className='tracks' key={track.id}>
-                    <p className='track'>{track.title}</p>
-
-                </div>)}))
+            // key={track.id}
+            <li class="list-group-item">{track.title}
+                <button type="button" class="btn btn-secondary track__play__button">Play</button>
+            </li>
+        )}))
 
 
     render() {
 
         return (
-            <div className="album__item">
-                <h2>
-                    {this.state.album.title}
-                </h2>
-                <img className="album_cover" src={this.state.album.cover}/>
-
-                <div className="track_item">
+            <div className="thumbnail album__container">
+                <h4 className="text-center"> {this.state.album.title}</h4>
+                <img  src={this.state.album.cover} className="img-responsive cover__album__tracks"/>
+                <ul class="list-group list-group-flush">
                     {this.state.tracks}
-                </div>
+                </ul>
+
             </div>
+
+
+
         )
     }
 
